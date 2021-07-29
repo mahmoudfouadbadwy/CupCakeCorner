17536ad4dd8f28fdbc532639ddf16c8c2250d440
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2021-07-28 17:49:46 -0700

    Initial commit

---
+++ CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Mahmoud Fouad on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
